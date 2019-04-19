e8e3b31f63bf6571432c22ead21d234ef150ec42
David Podhola <david@podhola.net>
2019-04-19 20:21:57 +0200

    0.28.0

--- merge.sh
+++ merge.sh
@@ -5,14 +5,14 @@ set -o pipefail
 
 merge()
 {
-    (cd $1 && pwd && (git add -A && git commit -m "version bump" || true ) && git checkout master-github && git merge --squash master --allow-unrelated-histories && (git commit -m "0.27.0" || true ) )
+    (cd $1 && pwd && (git add -A && git commit -m "version bump" || true ) && git checkout master-github && git merge --squash master --allow-unrelated-histories && (git commit -m "0.28.0" || true ) )
 }
 
 
-grep -rl '<idempiere-micro.version>0.25.0' ./**/pom.xml | xargs -r sed -i 's/<idempiere-micro.version>0.25.0/<idempiere-micro.version>0.27.0/g' || true
-grep -rl '<idempiere-micro.version>0.26.0' ./**/pom.xml | xargs -r sed -i 's/<idempiere-micro.version>0.26.0/<idempiere-micro.version>0.27.0/g' || true
-grep -rl 'IDEMPIERE_MICRO_VERSION: 0.25.0' ./**/.circleci/config.yml | xargs -r sed -i 's/IDEMPIERE_MICRO_VERSION: 0.25.0/IDEMPIERE_MICRO_VERSION: 0.27.0/g' || true 
-grep -rl 'IDEMPIERE_MICRO_VERSION: 0.26.0' ./**/.circleci/config.yml | xargs -r sed -i 's/IDEMPIERE_MICRO_VERSION: 0.26.0/IDEMPIERE_MICRO_VERSION: 0.27.0/g' || true
+grep -rl '<idempiere-micro.version>0.26.0' ./**/pom.xml | xargs -r sed -i 's/<idempiere-micro.version>0.26.0/<idempiere-micro.version>0.28.0/g' || true
+grep -rl '<idempiere-micro.version>0.27.0' ./**/pom.xml | xargs -r sed -i 's/<idempiere-micro.version>0.27.0/<idempiere-micro.version>0.28.0/g' || true
+grep -rl 'IDEMPIERE_MICRO_VERSION: 0.26.0' ./**/.circleci/config.yml | xargs -r sed -i 's/IDEMPIERE_MICRO_VERSION: 0.26.0/IDEMPIERE_MICRO_VERSION: 0.28.0/g' || true 
+grep -rl 'IDEMPIERE_MICRO_VERSION: 0.27.0' ./**/.circleci/config.yml | xargs -r sed -i 's/IDEMPIERE_MICRO_VERSION: 0.27.0/IDEMPIERE_MICRO_VERSION: 0.28.0/g' || true
 
 merge idempiere-micro-session-core
 merge idempiere-micro-base-interfaces
